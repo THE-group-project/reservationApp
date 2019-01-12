cd23620ab9d5e8a46bb1a54d8fed3b3a0e67678d
Finishoff <billvinhlam@gmail.com>
2019-01-12 11:26:56 -0800

    server.js

---
+++ data/tableData.js
@@ -0,0 +1,9 @@
+var tableArray = [
+    {
+      customerName: "Ferrin",
+      customerEmail: "Ferin@example.com",
+      customerID: "Ferrin",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+  module.exports = tableArray;
